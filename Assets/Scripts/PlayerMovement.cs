e2ca36fb0a0e624d4c19fc7cd9490cbfb94fc9c4
Grant <grantguenter@gmail.com>
2020-08-27 09:59:30 -0700

    Initial Unity Project Creation
    Creating base HDRP project trimmed of most example assets. Some assets left for material setup reference.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public CharacterController controller;
+
+    public float speed = 1f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 20f;
+
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 0.6f;
+
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -.3f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+}
