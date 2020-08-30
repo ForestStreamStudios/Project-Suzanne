519c0682cb2f7f3762109ca3e7e6424adede2a15
Grant <grantguenter@gmail.com>
2020-08-30 15:17:02 -0700

    Basic Navmesh example
    Implemented a basic navmesh agent that follows the player object on the navmesh.

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+    private NavMeshAgent agent;
+    private float speed;
+
+    void Start()
+    {
+        
+        agent = GetComponent<NavMeshAgent>();
+        speed = agent.speed;
+        agent.destination = goal.position;
+    }
+
+    private void Update()
+    {
+        agent.destination = goal.position;
+    }
+}
+
