e839d569df3de7f2074b8b618cca316a666b255d
Speterius <peet.seres@gmail.com>
2020-08-30 10:24:06 +0200

    Added pick up abstract class to trigger an effect. Added speed up power up example.

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PickupObject : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            PickUpEffect(collider.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    protected abstract void PickUpEffect(GameObject player); 
+
+}
---
+++ Assets/Scripts/SpeedPowerUpObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUpObject : PickupObject
+{
+
+    public float speedMultiplier = 2f;
+    public float speedUpDuration = 5f;
+
+    protected override void PickUpEffect(GameObject player) 
+    {
+        Debug.Log("Speed up effect triggered.");
+
+        player.GetComponent<PlayerMovement>().speed *= speedMultiplier;    
+    }
+}
