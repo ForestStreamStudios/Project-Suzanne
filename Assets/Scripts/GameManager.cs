beb6eb167778997e1a71d317b38501d9e37abbd6
Alex Sindledecker <alexsindledecker1@gmail.com>
2020-08-30 19:49:55 -0700

    Added player object that can move and look around with a testing scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action OnGamePause;
+    public event Action OnGameResume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnResume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Call this method if a pause button or key is pressed
+    public void OnPause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        if (OnGamePause != null)
+            OnGamePause();
+    }
+
+    public void OnResume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        if (OnGameResume != null)
+            OnGameResume();
+    }
+}
---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        xRotation = Mathf.Clamp(xRotation - mouseY, -90f, 90f);
+
+        transform.Rotate(0, mouseX, 0);
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
