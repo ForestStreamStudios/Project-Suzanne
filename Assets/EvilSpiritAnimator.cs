ad40db270f70c61a59d5c1277c1a7e08d24d57db
Speterius <peet.seres@gmail.com>
2020-09-02 18:48:19 +0200

    Added evil spirit placeholder.

---
+++ Assets/EvilSpiritAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EvilSpiritAnimator : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            animator.SetTrigger("toggleChomping");
+        }
+    }
+}
