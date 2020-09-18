ff33f974df88154d3805cd058cc57d1ef2a4f4b7
AwesomeReid <awesomereid11@yahoo.com>
2020-09-17 19:20:41 -0700

    Added sprint cooldown

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
     public float sprintModifier = 1.5f;
+    [Range(0,100)]
+    public float stamina = 100;
+    public int sprintRegen = 25;
 
     private Vector3 velocity;
     private CharacterController controller;
@@ -33,8 +36,16 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            verticalSpeed *= sprintModifier;
+            if (stamina > 0)
+            {
+                verticalSpeed *= sprintModifier;
+                stamina -= 75 * Time.deltaTime;
+            }
+            else {
+                stamina = -50;
+            }
         }
+        stamina = Mathf.Clamp(stamina + (sprintRegen * Time.deltaTime), -50, 100);
 
         float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
