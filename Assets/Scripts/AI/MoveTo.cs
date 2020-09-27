48246e26f9da59edceb8aa94b4253e8192b3a259
Baris SOYKOK <baris0103@gmail.com>
2020-09-27 20:15:25 +0300

    Revert "Ai speed up when player is looking at certain objects"
    This reverts commit 71c1add5ae16afb3e567f480fe2551f3386830a3.

--- Assets/Scripts/AI/MoveTo.cs
+++ Assets/Scripts/AI/MoveTo.cs
@@ -7,39 +7,19 @@ public class MoveTo : MonoBehaviour
 {
     public Transform goal;
     private NavMeshAgent agent;
-    private float speed, defaultSpeed;
+    private float speed;
 
     void Start()
     {
-
+        
         agent = GetComponent<NavMeshAgent>();
         speed = agent.speed;
-        defaultSpeed = speed;
         agent.destination = goal.position;
     }
 
     private void Update()
     {
         agent.destination = goal.position;
-    }
-
-    public void SetAiSpeed(float newSpeed)
-    {        
-        speed = newSpeed;
-        agent.speed = speed;
-        //Debug.Log("Ai Speeding Up...");
-    }
-
-    public float GetAiSpeed()
-    {
-        return speed;
-    }
-
-    public void ResetAiSpeed()
-    {
-        speed = defaultSpeed;
-        agent.speed = speed;
-        //Debug.Log("Ai Resetting Speed...");
     }
 }
 
