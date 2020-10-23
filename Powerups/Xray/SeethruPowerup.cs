f13940f265b8edaad5244f80a51de803e5624540
Andorthur1 <69833670+Andorthur1@users.noreply.github.com>
2020-10-23 23:54:37 +0530

    Add files via upload

---
+++ Powerups/Xray/SeethruPowerup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeethruPowerup : PickupObject
+{
+    protected override void PickUpEffect(GameObject player)
+    {
+        throw new System.NotImplementedException();
+        XrayWalls.isEffectActive = true;
+    }
+}
